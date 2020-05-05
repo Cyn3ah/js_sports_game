c92bfdab04f4095534dac36ac79d83c617b486de
Cyn3ah <62955916+Cyn3ah@users.noreply.github.com>
2020-05-05 04:55:36 -0500

    Not working, but...

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,4 @@
-// The best-practice is to pull all the elements into variables, in order to avoid
-// searching the HTML tree for these elements more than once:
+
 let teamOneShots = document.querySelector("#teamone-numshots"); 
 let teamOneGoals = document.querySelector("#teamone-numgoals"); 
 let teamOneShootButton = document.querySelector ("#teamone-shoot-button")
@@ -8,38 +7,22 @@ let teamTwoShots = document.querySelector("#teamtwo-numshots");
 let teamTwoGoals = document.querySelector("#teamtwo-numgoals"); 
 let teamTwoShootButton = document.querySelector ("#teamtwo-shoot-button") 
 
+let resetButton = document.querySelector("#reset-button")
 
 
-
-
-// This line says, "I want to listen for someone to CLICK my
-// incrementButton, and any time I 'hear' that event occur,
-// I want some code to run.
 teamOneShootButton.addEventListener("click", function() {
-    // Any code you write in here, will run every time incrementButton
-    // is clicked.
-    // Run the console.log function to display some text in the console:
-    console.log("+ button clicked");
-  }) // We have to close the curly braces and parentheses we opened above.
+    
+    let newTeamOneShots = Number(teamOneShots.innerHTML) + 1;
+    
+    teamOneShots.innerHTML = newTeamOneShots;
 
-    // This line says, "I want to listen for someone to CLICK my
-    // incrementButton, and any time I 'hear' that event occur,
-    // I want some code to run.
-    teamTwoShootButton.addEventListener("click", function() {
-    // Any code you write in here, will run every time incrementButton
-    // is clicked.
-    // Run the console.log function to display some text in the console:
-    console.log("+ button clicked");
-  }) // We have to close the curly braces and parentheses we opened above.
+  }) 
+  
+teamTwoShootButton.addEventListener("click", function() {
+   
+    let newTeamTwoShots = Number(teamTwoShootButton.innerHTML) + 1;
+  
+    teamTwoShots.innerHTML = newTeamTwoShots;
 
-  teamOneShootButton.addEventListener("click", function() {
-    console.log("+ button clicked");
-    // Calculate the new value for our counter:
-    let teamOneShots = Number(teamOneShootButton.innerHTML) + 1;
   })
 
-    teamTwoShootButton.addEventListener("click", function() {
-    console.log("+ button clicked");
-    // Calculate the new value for our counter:
-    let teamTwoShots = Number(teamTwoShootButton.innerHTML) + 1;
-  })
