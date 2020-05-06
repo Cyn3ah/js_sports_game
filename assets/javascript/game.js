25d994962cc9819d52a98d655ed2737b0da19b3a
Cyn3ah <62955916+Cyn3ah@users.noreply.github.com>
2020-05-05 21:06:28 -0500

    Made it Pretty, but broke my buttons

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,57 +1,57 @@
 
-let teamOneShots = document.querySelector("#teamone-numshots"); 
-let teamOneGoals = document.querySelector("#teamone-numgoals"); 
-let teamOneShootButton = document.querySelector ("#teamone-shoot-button");
+let teamOneShots = document.querySelector("#teamone-numshots");
+let teamOneGoals = document.querySelector("#teamone-numgoals");
+let teamOneShootButton = document.querySelector("#teamone-shoot-button");
 
-let teamTwoShots = document.querySelector("#teamtwo-numshots"); 
-let teamTwoGoals = document.querySelector("#teamtwo-numgoals"); 
-let teamTwoShootButton = document.querySelector ("#teamtwo-shoot-button");
+let teamTwoShots = document.querySelector("#teamtwo-numshots");
+let teamTwoGoals = document.querySelector("#teamtwo-numgoals");
+let teamTwoShootButton = document.querySelector("#teamtwo-shoot-button");
 
 let resetButton = document.querySelector("#reset-button");
 let numResets = document.querySelector("#num-resets");
 
-resetButton.addEventListener("click", function() {
+resetButton.addEventListener("click", function () {
 
-    let newTeamOneShots = 0;
-    teamOneShots.innerHTML = newTeamOneShots;
+  let newTeamOneShots = 0;
+  teamOneShots.innerHTML = newTeamOneShots;
 
-    let newTeamOneGoals = 0;
-    teamOneGoals.innerHTML = newTeamOneGoals;
+  let newTeamOneGoals = 0;
+  teamOneGoals.innerHTML = newTeamOneGoals;
 
-    let newTeamTwoShots = 0;
-    teamTwoShots.innerHTML = newTeamTwoShots;
+  let newTeamTwoShots = 0;
+  teamTwoShots.innerHTML = newTeamTwoShots;
 
-    let newTeamTwoGoals = 0;
-    teamTwoGoals.innerHTML = newTeamTwoGoals;
+  let newTeamTwoGoals = 0;
+  teamTwoGoals.innerHTML = newTeamTwoGoals;
 
-    let newNumResets = Number(numResets.innerHTML) + 1;
+  let newNumResets = Number(numResets.innerHTML) + 1;
 
-    numResets.innerHTML = newNumResets;
+  numResets.innerHTML = newNumResets;
 })
 
-teamOneShootButton.addEventListener("click", function() {
-    
-    let newTeamOneShots = Number(teamOneShots.innerHTML) + 1;
-    
-    teamOneShots.innerHTML = newTeamOneShots;
+teamOneShootButton.addEventListener("click", function () {
 
-    let newTeamOneGoals = Number(teamOneGoals.innerHTML) + (Math.random() < 0.5);
+  let newTeamOneShots = Number(teamOneShots.innerHTML) + 1;
 
-    teamOneGoals.innerHTML = newTeamOneGoals;
+  teamOneShots.innerHTML = newTeamOneShots;
 
-  }) 
-  
-teamTwoShootButton.addEventListener("click", function() {
-   
-    let newTeamTwoShots = Number(teamTwoShots.innerHTML) + 1;
-  
-    teamTwoShots.innerHTML = newTeamTwoShots;
+  let newTeamOneGoals = Number(teamOneGoals.innerHTML) + (Math.random() < 0.5);
 
-    let newTeamTwoGoals = Number(teamTwoGoals.innerHTML) + (Math.random() < 0.5);
+  teamOneGoals.innerHTML = newTeamOneGoals;
 
-    teamTwoGoals.innerHTML = newTeamTwoGoals;
+})
 
-  })
+teamTwoShootButton.addEventListener("click", function () {
+
+  let newTeamTwoShots = Number(teamTwoShots.innerHTML) + 1;
+
+  teamTwoShots.innerHTML = newTeamTwoShots;
+
+  let newTeamTwoGoals = Number(teamTwoGoals.innerHTML) + (Math.random() < 0.5);
+
+  teamTwoGoals.innerHTML = newTeamTwoGoals;
+
+})
 
 //   Clicking the "RESET" button should reset all the shot and goal counters and add 1 to the number of resets
 
